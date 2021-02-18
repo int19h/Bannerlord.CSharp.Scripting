ecf5e94917a939bc4dbc10810bcfa9052148d359
int19h <me@int19h.org>
2021-02-18 12:09:09 -0800

    Add action helper to kill heroes.

---
+++ ScriptGlobals.ActionHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+
+namespace Int19h.Bannerlord.CSharp.Scripting {
+    partial class ScriptGlobals {
+        public static void Kill(this Hero hero) => KillCharacterAction.ApplyByMurder(hero);
+
+        public static void Kill(this IEnumerable<Hero> heroes) {
+            foreach (var hero in heroes) {
+                hero.Kill();
+            }
+        }
+
+        public static void Kill(params Hero[] heroes) => heroes.Kill();
+    }
+}
