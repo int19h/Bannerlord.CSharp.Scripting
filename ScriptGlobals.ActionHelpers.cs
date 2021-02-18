91945d65919ecff1ef45ee83a29eb1359de226e5
int19h <me@int19h.org>
2021-02-18 12:14:21 -0800

    Add action helpers for pregnancy.

--- ScriptGlobals.ActionHelpers.cs
+++ ScriptGlobals.ActionHelpers.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Actions;
 
 namespace Int19h.Bannerlord.CSharp.Scripting {
     partial class ScriptGlobals {
-        public static void Kill(this Hero hero) => KillCharacterAction.ApplyByMurder(hero);
+        public static void Kill(this Hero hero) {
+            if (hero.IsDead) {
+                throw new InvalidOperationException($"{hero} is already dead");
+            }
+            KillCharacterAction.ApplyByMurder(hero);
+        }
 
         public static void Kill(this IEnumerable<Hero> heroes) {
             foreach (var hero in heroes) {
@@ -13,5 +19,22 @@ namespace Int19h.Bannerlord.CSharp.Scripting {
         }
 
         public static void Kill(params Hero[] heroes) => heroes.Kill();
+
+        public static void MakePregnant(this Hero hero) {
+            if (hero.IsPregnant) {
+                throw new InvalidOperationException($"{hero} is already pregnant");
+            } else if (hero.Spouse == null) {
+                throw new InvalidOperationException($"{hero} has no spouse");
+            }
+            MakePregnantAction.Apply(hero);
+        }
+
+        public static void MakePregnant(this IEnumerable<Hero> heroes) {
+            foreach (var hero in heroes) {
+                hero.MakePregnant();
+            }
+        }
+
+        public static void MakePregnant(params Hero[] heroes) => heroes.MakePregnant();
     }
 }
