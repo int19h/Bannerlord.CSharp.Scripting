3a3a797dd90c731b371935430b21ed784df072e0
int19h <me@int19h.org>
2021-02-18 11:49:30 -0800

    Rename EnumerableWithLookup -> LookupTable.

--- LookupTable.cs
+++ LookupTable.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TaleWorlds.CampaignSystem;
 
 namespace Int19h.Bannerlord.CSharp.Scripting {
     public interface ILookupTable<out T> : IEnumerable<T> {
         T this[Lookup lookup] { get; }
+    }
+
+    public struct LookupTable<T> : ILookupTable<T>
+        where T : class {
+
+        private readonly IEnumerable<T> source;
+
+        public LookupTable(IEnumerable<T> source) {
+            this.source = source;
+        }
+
+        public IEnumerator<T> GetEnumerator() => source.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public static implicit operator T[](LookupTable<T> en) => en.ToArray();
+
+        public T this[Lookup lookup] {
+            get {
+                var items = this.Where(lookup.Matches).ToArray();
+                if (items.Length == 1) {
+                    return items[0];
+                } else if (items.Length == 0) {
+                    throw new KeyNotFoundException($"No {typeof(T).Name} with {lookup.What} == '{lookup}'");
+                } else {
+                    throw new KeyNotFoundException($"More than one {typeof(T).Name} with {lookup.What} == '{lookup}'");
+                }
+            }
+        }
+
+        public T[] this[params Lookup[] lookups] {
+            get {
+                var self = this;
+                return lookups.Select(lookup => self[lookup]).ToArray();
+            }
+        }
     }
 
     internal struct LookupTables {
--- ScriptGlobals.LookupHelpers.cs
+++ ScriptGlobals.LookupHelpers.cs
@@ -8,22 +8,22 @@ namespace Int19h.Bannerlord.CSharp.Scripting {
 
         public static Hero MainHero => Hero.MainHero;
 
-        public static EnumerableWithLookup<Kingdom> Kingdoms => new(Kingdom.All);
+        public static LookupTable<Kingdom> Kingdoms => new(Kingdom.All);
 
-        public static EnumerableWithLookup<Clan> Clans => new(Clan.All);
+        public static LookupTable<Clan> Clans => new(Clan.All);
 
-        public static EnumerableWithLookup<Hero> Heroes => new(Hero.All);
+        public static LookupTable<Hero> Heroes => new(Hero.All);
 
-        public static EnumerableWithLookup<Settlement> Settlements => new(Settlement.All);
+        public static LookupTable<Settlement> Settlements => new(Settlement.All);
 
-        public static EnumerableWithLookup<Town> Fiefs => new(Town.AllFiefs);
+        public static LookupTable<Town> Fiefs => new(Town.AllFiefs);
 
-        public static EnumerableWithLookup<Town> Towns => new(Town.AllTowns);
+        public static LookupTable<Town> Towns => new(Town.AllTowns);
 
-        public static EnumerableWithLookup<Town> Castles => new(Town.AllCastles);
+        public static LookupTable<Town> Castles => new(Town.AllCastles);
 
-        public static EnumerableWithLookup<Village> Villages => new(Village.All);
+        public static LookupTable<Village> Villages => new(Village.All);
 
-        public static EnumerableWithLookup<MobileParty> Parties => new(MobileParty.All);
+        public static LookupTable<MobileParty> Parties => new(MobileParty.All);
     }
 }
