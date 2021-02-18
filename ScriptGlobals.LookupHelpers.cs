7da3c77f97f7b095ce0eb63a642fa18eebe9283b
int19h <me@int19h.org>
2021-02-18 12:10:08 -0800

    Add lookup helpers for nobles and wanderers.
    Add lookup helpers for hero's descendants, friends, and enemies.
    
    Rename MainHero -> Me for conciseness, and add a bunch of helper shortcuts for various properties of Me.

--- ScriptGlobals.LookupHelpers.cs
+++ ScriptGlobals.LookupHelpers.cs
@@ -1,4 +1,6 @@
-﻿using TaleWorlds.CampaignSystem;
+﻿using System.Collections.Generic;
+using System.Linq;
+using TaleWorlds.CampaignSystem;
 
 namespace Int19h.Bannerlord.CSharp.Scripting {
     partial class ScriptGlobals {
@@ -6,13 +8,15 @@ namespace Int19h.Bannerlord.CSharp.Scripting {
 
         public static IdLookup Id(string id) => new IdLookup(id);
 
-        public static Hero MainHero => Hero.MainHero;
-
         public static LookupTable<Kingdom> Kingdoms => new(Kingdom.All);
 
         public static LookupTable<Clan> Clans => new(Clan.All);
 
         public static LookupTable<Hero> Heroes => new(Hero.All);
+
+        public static LookupTable<Hero> Nobles => new(Heroes.Where(hero => hero.IsNoble));
+
+        public static LookupTable<Hero> Wanderers => new(Heroes.Where(hero => hero.IsWanderer));
 
         public static LookupTable<Settlement> Settlements => new(Settlement.All);
 
@@ -25,5 +29,18 @@ namespace Int19h.Bannerlord.CSharp.Scripting {
         public static LookupTable<Village> Villages => new(Village.All);
 
         public static LookupTable<MobileParty> Parties => new(MobileParty.All);
+
+        public static IEnumerable<Hero> Descendants(this Hero hero) {
+            foreach (var child in hero.Children) {
+                yield return child;
+                foreach (var descendant in child.Descendants()) {
+                    yield return descendant;
+                }
+            }
+        }
+
+        public static IEnumerable<Hero> Friends(this Hero hero) => Heroes.Where(other => other.IsFriend(hero));
+
+        public static IEnumerable<Hero> Enemies(this Hero hero) => Heroes.Where(other => other.IsEnemy(hero));
     }
 }
---
+++ ScriptGlobals.Me.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TaleWorlds.CampaignSystem;
+
+namespace Int19h.Bannerlord.CSharp.Scripting {
+    partial class ScriptGlobals {
+        public static Hero Me => Hero.MainHero;
+
+        public static Clan MyClan => Me.Clan;
+
+        public static Kingdom? MyKingdom => Me.Clan.Kingdom;
+
+        public static IEnumerable<Town> MyFiefs => Me.Clan.Fiefs;
+
+        public static IEnumerable<Town> MyTowns => MyFiefs.Where(fief => fief.IsTown);
+
+        public static IEnumerable<Town> MyCastles => MyFiefs.Where(fief => fief.IsCastle);
+
+        public static IEnumerable<Village> MyVillages => Me.Clan.Villages;
+
+        public static Hero? MySpouse => Me.Spouse;
+
+        public static IEnumerable<Hero> MyCompanions => Me.Clan.Companions;
+
+        public static IEnumerable<Hero> MyFamily => Me.Clan.Lords;
+
+        public static IEnumerable<Hero> MyChildren => Me.Children;
+
+        public static IEnumerable<Hero> MyDescendants => Me.Descendants();
+
+        public static IEnumerable<Hero> MyFriends => Me.Friends();
+
+        public static IEnumerable<Hero> MyEnemies => Me.Enemies();
+
+        public static MobileParty MyParty => Me.PartyBelongedTo;
+
+        public static ItemRoster MyItems => MyParty.ItemRoster;
+    }
+}
