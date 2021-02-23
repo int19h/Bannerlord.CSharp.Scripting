95a69337f2c67bac09e1a9222e5770f50c16ced2
int19h <me@int19h.org>
2021-02-22 22:25:13 -0800

    Add CampaignBehavior.

---
+++ CampaignBehavior.cs
@@ -0,0 +1,11 @@
+﻿using TaleWorlds.CampaignSystem;
+
+namespace Int19h.Bannerlord.CSharp.Scripting {
+    public class CampaignBehavior : CampaignBehaviorBase {
+        public override void RegisterEvents() {
+        }
+
+        public override void SyncData(IDataStore dataStore) {
+        }
+    }
+}
--- SubModule.cs
+++ SubModule.cs
@@ -1,9 +1,19 @@
-﻿using TaleWorlds.MountAndBlade;
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
 
 namespace Int19h.Bannerlord.CSharp.Scripting {
     public class SubModule : MBSubModuleBase {
         protected override void OnSubModuleLoad() {
             ScriptFiles.Initialize();
+        }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject) {
+            base.OnGameStart(game, gameStarterObject);
+
+            if (gameStarterObject is CampaignGameStarter campaignStarter) {
+                campaignStarter.AddBehavior(new CampaignBehavior());
+            }
         }
     }
 }
