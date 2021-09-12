8dc5860e29d531d31a9a56da8f5642fc47a983e7
int19h <me@int19h.org>
2021-09-11 22:55:43 -0700

    Add helpers to ignore visibility at runtime.

--- ScriptGlobals.cs
+++ ScriptGlobals.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Dynamic;
 using System.IO;
+using System.Linq.Expressions;
 
 namespace Int19h.Bannerlord.CSharp.Scripting {
     public static partial class ScriptGlobals {
@@ -11,5 +12,21 @@ namespace Int19h.Bannerlord.CSharp.Scripting {
         public static readonly dynamic Shared = new ExpandoObject();
 
         public static string? ScriptPath { get; internal set; }
+
+        public static void IgnoreVisibility(Expression<Action> expr) => expr.Compile()();
+
+        public static T IgnoreVisibility<T>(Expression<Func<T>> expr) => expr.Compile()();
+
+        public static T Get<T>(Lookup lookup) {
+            var table = LookupTables.Get<T>();
+            if (table == null) {
+                throw new ArgumentException($"Unsupported type {typeof(T).Name}", nameof(T));
+            }
+            return table[lookup];
+        }
+        
+        public static void Set<T>(out T target, T value) {
+            target = value;
+        }
     }
 }
